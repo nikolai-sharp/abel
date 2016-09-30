29d1fccbe17dda074d5e37800a579f29bddaf431
Nikolai Sharp <nikolai.sharp@ucdenver.edu>
2016-09-30 13:56:30 -0600

    I finished the function for problem 1#

--- SA1_Abel.py
+++ SA1_Abel.py
@@ -5,42 +5,45 @@ Created on Fri Sep 30 09:38:08 2016
 @author: Liam
 """
 
-from numpy import math
-from sympy.solvers import solve
-from sympy import Symbol
-
-l1 = 2
-l2 = 2**(1/2)
-l3 = 2**(1/2)
-gamma = math.pi/2
-
-def findxy(p1,p2,p3,gamma,theta):
-#give the function the gamm and theta of pi/4
-#should return the x and y
-#set up a solver to find x and y.    
+import numpy as np
 
 
 
-def kinematics(p1, p2, p3, gamma):
-    from mpmath import *
-    mp.dps = 25; mp.pretty = True
-    z = Symbol('z')
-    a2 = l3*mpmath.cospi(z)-x1
-    b2 = l3*mpmath.sinpi(z)
-    a3 = l2*mpmath.cospi(z+gamma)-x2
-    b3 = l2*mpmath.sinpi(z+gamma)-y2
-    n1 = b3*(p2**2-p1**2-a2**2-b2**2)-b2*(p3**2-p1**2-a3**2-b3**2)
-    n2 = -a3*(p2**2-p1**2-a2**2-b2**2)-a2*(p3**2-p1**2-a3**2-b3**2)
-    d = 2*(a2*b3-b2*a3)
-    x = n1/d
-    y = n2/d
-    func= n1**2+n2**2-p1**2*d**2
-    solve(func, z)
-    return z
-    print z    
 
-kinematics(5.**(1/2),5.**(1/2),5.**(1/2), (1/2))
+def f(theta):
+    #l1 = 2.0
+    l2 = 2**(1.0/2)
+    l3 = l2
+    p1 = 5**(1.0/2)
+    p2 = p1 #saving calculations
+    p3 = p1
+    p1s = 5.0 #square root of 5 squared is 5.. no need to calculate this 12 times
+    p2s = 5.0
+    p3s = 5.0
+    gamma = (np.pi)/2
     
+    x1 = 4.0
+    y1 = 0.0
+    x2 = 0
+    y2 = 4.0
     
+    A2 = l3*np.cos(theta) - x1
+    B2 = l3*np.sin(theta) - y1
+    A3 = l2*(np.cos(theta)*np.cos(gamma)-np.sin(theta)*np.sin(gamma)) - x2
+    B3 = l2*(np.cos(theta)*np.sin(gamma)+np.sin(theta)*np.cos(gamma)) - y2
+    A2s = A2**2
+    B2s = B2**2
+    A3s = A3**2
+    B3s = B3**2
+    ppab2 = p2s-p1s-A2s-B2s
+    ppab3 = p3s-p1s-A3s-B3s
     
+    N1s = (B3*ppab2-B2*ppab3)**2
+    N2s = (A2*ppab3-A3*ppab2)**2  #I swapped the order so the negatives would match
     
+    Ds = 4*(A2*B3-A3*B2)**2
+    
+    return N1s + N2s -p1s*Ds
+    
+print f(np.pi/4)
+print np.cos(np.pi)
